e2cd41757988d56518e338b54733126ab603805d
Hoakin <Hoakin037@gmail.com>
2026-08-23 16:58:49 +0700

    fix: logging, add methods logging

--- app/modules/users/repositories/user_repo.py
+++ app/modules/users/repositories/user_repo.py
@@ -1,5 +1,6 @@
 from sqlalchemy.ext.asyncio import AsyncSession
 
+from app.infrastructure.logger import setup_logging
 from app.modules.users.interfaces import IUserRepo
 from app.modules.users.models import UserModel
 from app.modules.users.schemas.user import UserCreate, UserUpdate
@@ -8,4 +9,6 @@ from app.shared.repositories import PostgresBaseRepo
 
 class UserRepo(PostgresBaseRepo[UserModel, UserCreate, UserUpdate], IUserRepo):
     def __init__(self, session: AsyncSession):
-        super().__init__(session=session, model=UserModel)
+        super().__init__(
+            session=session, model=UserModel, logger=setup_logging(__name__)
+        )
--- app/shared/repositories/redis/redis_base_repo.py
+++ app/shared/repositories/redis/redis_base_repo.py
@@ -2,22 +2,30 @@ from datetime import timedelta
 
 from redis.asyncio.client import Redis
 
+from app.infrastructure.decorators import logg_function
+from app.infrastructure.logger import setup_logging
+
 
 class RedisBaseRepository:
     def __init__(self, client: Redis):
         self.client = client
+        self._logger = setup_logging(__name__)
 
+    @logg_function(description="Get instance from redis by key")
     async def get_instance(self, key: str) -> bytes | str | None:
         return await self.client.get(key)
 
+    @logg_function(description="Set instance to redis")
     async def set_instance(
         self, key: str, value: bytes | str | None, expire_time: int | timedelta = None
     ) -> None:
         await self.client.set(name=key, value=value, ex=expire_time)
 
+    @logg_function(description="Delete instance from redis by key")
     async def delete_instance(self, key: str) -> None:
         await self.client.delete(key)
 
+    @logg_function(description="Delete instance from redis by prefix")
     async def delete_by_prefix(self, prefix: str) -> None:
         cursor = b"0"
         pattern = f"{prefix}*"
@@ -35,5 +43,6 @@ class RedisBaseRepository:
             if cursor == b"0":
                 break
 
-    async def check_existance(self, key: str) -> bool:
+    @logg_function(description="Check instance existence in redis")
+    async def check_existence(self, key: str) -> bool:
         return await self.client.exists(key) > 0
